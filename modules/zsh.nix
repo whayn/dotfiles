0fad969d51be5adebd75c6d58e91293e82e992b1
Whayn <whayn@users.noreply.github.com>
2026-04-04 22:00:51 +0200

    feat/zsh: some bindings added

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -34,6 +34,15 @@
 
       PROMPT="%n@%m:%~ > "
 
+      # some fukcin bindings
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
+      bindkey "^[[1;3C" forward-word
+      bindkey "^[[1;3D" backward-word
+      bindkey "^[[H" beginning-of-line
+      bindkey "^[[F" end-of-line
+      bindkey "^[[3~" delete-char
+
       # Syntax higlighting
       source ${
         pkgs.fetchFromGitHub {
