4ab095562ca89299a92229049302f1977110f461
Whayn <whayn@users.noreply.github.com>
2026-04-04 21:31:52 +0200

    fix/zsh: pretty rainbow

--- assets/rainbow-prompt.zsh
+++ assets/rainbow-prompt.zsh
@@ -1,15 +1,29 @@
 # Enable dynamic prompt substitution
 setopt PROMPT_SUBST
+zmodload zsh/mathfunc
 
 build_rainbow_prompt() {
-  local raw_text="${USERNAME}@${HOST%%.*}:${PWD/#$HOME/~} > "
-  local colors=(red yellow green cyan blue magenta)
+  local raw_text="${USER:-$USERNAME}@${HOST%%.*}:${PWD/#$HOME/~} > "
   local new_prompt=""
+  local len=${#raw_text}
+  (( len == 0 )) && len=1
 
-  for (( i=0; i < ${#raw_text}; i++ )); do
+  # Spread the rainbow spectrum across the entire string length
+  local freq=$(( 6.28318 / len ))
+
+  for (( i=0; i < len; i++ )); do
     local char="${raw_text:$i:1}"
-    local color="${colors[((i % 6) + 1)]}"
-    new_prompt+="%F{${color}}${char}"
+
+    # Calculate smooth RGB values using sine waves offset by 120 degrees (2pi/3)
+    local r=$(( int(sin(freq * i + 0) * 127 + 128) ))
+    local g=$(( int(sin(freq * i + 2.0944) * 127 + 128) ))
+    local b=$(( int(sin(freq * i + 4.1888) * 127 + 128) ))
+
+    # Format directly as a zsh truecolor code %F{#RRGGBB}
+    # Using printf -v avoids subshell performance penalties
+    local hex
+    printf -v hex "%%F{#%02x%02x%02x}" $r $g $b
+    new_prompt+="${hex}${char}"
   done
 
   PROMPT="${new_prompt}%f"
